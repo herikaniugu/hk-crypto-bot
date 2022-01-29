7626522a1ccf66c7f9c450601de4250e08c42f20
Heri Kaniugu <hekan12@gmail.com>
2022-01-29 19:38:47 +0300

    Updated

--- server/trade.js
+++ server/trade.js
@@ -25,54 +25,24 @@ module.exports = (request, response) => {
     if (["long", "short"].indexOf(action) >= 0) return exchange.fetchPositions().then(async (positions) => {
         // AVAILABLE
         const available = positions.filter((item) => item.info.symbol === symbol && item.entryPrice > 0).length === 0;
-        response.json({ available: available, positions: positions });
+        // VIEW
+        if (available) return await exchange.fapiPrivateGetOpenOrders(pair).then(async (orders) => {
+            // CANCEL
+            return await Promise.all(orders.filter((order) => order.symbol === symbol).map((order) => exchange.cancelOrder(order.orderId, order.symbol))).then(async () => {
+                // ORDER
+                response.json({ available: available, positions: positions, orders: orders });
+                // // ORDER
+                // return await exchange.createOrder(pair, "MARKET", action.type === "long" ? "BUY" : "SELL", precision(amount / price, decimal.amount), undefined, { positionSide: "BOTH" }).then(async (order) => {
+                //     // return await exchange.createOrder(pair, "TRAILING_STOP_MARKET", order.side === "buy" ? "SELL" : "BUY", order.amount, resistance, { activationPrice: resistance, callbackRate: 0.08 * 3 }).then(() => {
+                //     return await exchange.createOrder(pair, "TAKE_PROFIT_MARKET", order.side === "buy" ? "SELL" : "BUY", order.amount, resistance, { closePosition: true, stopPrice: resistance }).then(async () => {
+                //         return await exchange.createOrder(pair, "STOP_MARKET", order.side === "buy" ? "SELL" : "BUY", order.amount, support, { closePosition: true, stopPrice: support }).then(async () => {
+                //             console.log(runtime, { pair: pair, order: order });
+                //         });
+                //     });
+                // });
+            });
+        });
+        response.json(request.body);
     });
     response.json(request.body);
 };
-
-    
-    /*
-    // PAIR
-    const pair = [quote, base].join("/");
-    const symbol = [quote, base].join("");
-    // RUNTIME
-    runtime += 1;
-    // DATA
-    exchange.fetchOHLCV(pair, "1m").then(async (data) => {
-        // STRATEGY
-        const action = strategy(data.map((item) => item[4]));
-        // CONFIRMATION
-        const confirmation = ["long", "short"].indexOf(action.type) >= 0;
-        // POSITION
-        if (confirmation) return await exchange.fetchPositions().then(async (positions) => {
-            // PRICE
-            const price = action.price;
-            // RESISTANCE
-            const resistance = action.resistance;
-            // SUPPORT
-            const support = action.support;
-            // AVAILABLE
-            const available = positions.filter((item) => item.info.symbol === symbol && item.entryPrice > 0).length === 0;
-            // VIEW
-            if (available) return await exchange.fapiPrivateGetOpenOrders(pair).then(async (orders) => {
-                // CANCEL
-                return await Promise.all(orders.filter((order) => order.symbol === symbol).map((order) => exchange.cancelOrder(order.orderId, order.symbol))).then(async () => {
-                    // ORDER
-                    return await exchange.createOrder(pair, "MARKET", action.type === "long" ? "BUY" : "SELL", precision(amount / price, decimal.amount), undefined, { positionSide: "BOTH" }).then(async (order) => {
-                        // return await exchange.createOrder(pair, "TRAILING_STOP_MARKET", order.side === "buy" ? "SELL" : "BUY", order.amount, resistance, { activationPrice: resistance, callbackRate: 0.08 * 3 }).then(() => {
-                        return await exchange.createOrder(pair, "TAKE_PROFIT_MARKET", order.side === "buy" ? "SELL" : "BUY", order.amount, resistance, { closePosition: true, stopPrice: resistance }).then(async () => {
-                            return await exchange.createOrder(pair, "STOP_MARKET", order.side === "buy" ? "SELL" : "BUY", order.amount, support, { closePosition: true, stopPrice: support }).then(async () => {
-                                console.log(runtime, { pair: pair, order: order });
-                            });
-                        });
-                    });
-                });
-            });
-            console.log(runtime, { pair: pair, confirmation: true });
-        });
-        console.log(runtime, { pair: pair, confirmation: false });
-    }).catch((error) => {
-        // ERROR
-        console.log(error);
-    });
-    */
