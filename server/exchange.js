0f3dd7277676110fff174f7b11ec74980853cfa0
Heri Kaniugu <hekan12@gmail.com>
2022-01-31 16:18:18 +0300

    Initial Commit

--- server/exchange.js
+++ server/exchange.js
@@ -2,10 +2,8 @@ const ccxt = require("ccxt");
 
 // MAINNET
 const mainnet = new ccxt.binance({
-    apiKey: "t2FXKzeJjm4WlKqmfzt1qHmJfAp98kELICtuHTBmF8NouiEilwAWHJNxxMW92Iki",
-    secret: "MLRC835naVSV6pM2JAUXvLCcLeLcxWwxEQOgrVD2Joztm0oBtEmaHKqyFExaJR8B",
-    // apiKey: process.env.BINANCE_API_KEY,
-    // secret: process.env.BINANCE_API_SECRET,
+    apiKey: process.env.BINANCE_API_KEY,
+    secret: process.env.BINANCE_API_SECRET,
     options: { adjustForTimeDifference: true, defaultType: "future", hedgeMode: false }
 });
 
